01aab4e134424be739c4243ba9ed001c9b3669b9
wawan-ikhwan <ikhwanperwira@gmail.com>
2023-11-06 19:22:11 +0700

    test

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,4 @@ def index():
   return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run()
+  app.run(port=int(os.environ.get('PORT', 8000)))
