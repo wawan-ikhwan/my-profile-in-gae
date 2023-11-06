c59930b560daae0f1c150c0c85f6d16e67b74037
wawan-ikhwan <ikhwanperwira@gmail.com>
2023-11-07 03:10:05 +0700

    final

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  image_root_path = 'https://storage.googleapis.com/menjadi-gcloud-engineer.appspot.com/images/'
+  return render_template('index.html', image_root_path=image_root_path)
 
 if __name__ == '__main__':
   app.run(host="127.0.0.1", port=8080, debug=True)
