b415c933bceb004f1b77c6319aa8c1b17ceb7df3
wawan-ikhwan <ikhwanperwira@gmail.com>
2023-11-06 19:26:17 +0700

    testing

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def index():
   return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(port=int(os.environ.get('PORT', 8000)))
+  app.run(host="127.0.0.1", port=8080, debug=True)
